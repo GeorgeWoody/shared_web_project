02235ca6c948241f77fde1e4f0453e7adf0b0695
GeorgeWoody <depecheway@gmail.com>
2024-09-26 16:14:19 -0300

    Simple Classes Added

--- polls/models.py
+++ polls/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.Charfield(max_length=50)
+    pub_date = models.DateTimeField("Fecha de Publicación")
+    
+class Choice(models.Model):
+    question = models.ForeignKey(max_length=200)
+    choice_text = models.DateTimeField(Question, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
     
