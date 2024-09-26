d497b34ae7247d08418e4b1e5597edf4be6dee6a
GeorgeWoody <depecheway@gmail.com>
2024-09-26 16:31:12 -0300

    Poll Added to Settings

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Question(models.Model):
-    question_text = models.Charfield(max_length=50)
+    question_text = models.CharField(max_length=50)
     pub_date = models.DateTimeField("Fecha de Publicación")
     
 class Choice(models.Model):
-    question = models.ForeignKey(max_length=200)
-    choice_text = models.DateTimeField(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.DateTimeField(max_length=200)
     votes = models.IntegerField(default=0)
     
