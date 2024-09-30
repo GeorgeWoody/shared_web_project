093fe258aa43c5121deabc3575144d6caf3eb9b0
GeorgeWoody <depecheway@gmail.com>
2024-09-30 13:24:44 -0300

    Context Added

--- polls/models.py
+++ polls/models.py
@@ -17,7 +17,6 @@ class Choice(models.Model):
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
-    
+        
     def __str__(self):
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Proyecto subido a GitHub - app Polls")
+    lates_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "lates_question_list" : lates_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("Your're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "Your're looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("your're voting on question %s." % question_id)
+
