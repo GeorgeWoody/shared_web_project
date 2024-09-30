cc30b5a33f5be7486f177070ca5d7f2ec8bc3ebc
GeorgeWoody <depecheway@gmail.com>
2024-09-30 13:28:19 -0300

    Context Added 02

--- polls/views.py
+++ polls/views.py
@@ -6,10 +6,10 @@ from .models import Question
 
 
 def index(request):
-    lates_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
     template = loader.get_template("polls/index.html")
     context = {
-        "lates_question_list" : lates_question_list,
+        "lates_question_list" : latest_question_list,
     }
     return HttpResponse(template.render(context, request))
 
