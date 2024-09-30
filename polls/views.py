c4fd519502f76430ec6fd85b49b8571d877b82df
GeorgeWoody <depecheway@gmail.com>
2024-09-30 15:33:28 -0300

    Using Render

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -7,15 +7,17 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "lates_question_list" : latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    template = loader.get_template
+    context = {"latest_question_list" : latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("Your're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question":question})
 
 def results(request, question_id):
     response = "Your're looking at the result of question %s"
